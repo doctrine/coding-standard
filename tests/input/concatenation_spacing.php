cdce50da5a62b60640a43aacfa7abe8f5268ada9
Luís Cobucci <lcobucci@gmail.com>
2017-08-20 01:19:27 +0200

    Use PHPCS itself to validate our sniffs
    That's easier than doing some workarounds on PHPUnit.

---
+++ tests/input/concatenation_spacing.php
@@ -0,0 +1,35 @@
+<?php
+
+$string = 'Doctrine' . 'rulez';
+$string = 'Doctrine'. 'rulez';
+$string = 'Doctrine' .'rulez';
+$string = 'Doctrine'.'rulez';
+
+$doctrine = 'Doctrine';
+$rulez    = 'rulez';
+$string   = $doctrine . $rulez;
+$string   = $doctrine. $rulez;
+$string   = $doctrine .$rulez;
+$string   = $doctrine.$rulez;
+
+$string = 'Doctrine' . $rulez;
+$string = 'Doctrine'. $rulez;
+$string = 'Doctrine' .$rulez;
+$string = 'Doctrine'.$rulez;
+
+$string = $doctrine . 'rulez';
+$string = $doctrine .           'rulez';
+$string = $doctrine. 'rulez';
+$string = $doctrine .'rulez';
+$string = $doctrine.'rulez';
+
+$string = $doctrine . $rulez. 'even' .'more'.$string;
+
+$string.=$doctrine;
+$string .= $doctrine;
+$string .=$doctrine;
+
+$string = $doctrine.
+    $rulez .
+    'all'
+    .$string;
---
+++ tests/input/return_type_on_closures.php
@@ -0,0 +1,66 @@
+<?php
+
+function ():void
+{
+}
+
+function (): void
+{
+}
+
+function ():   void
+{
+}
+
+function () :void
+{
+}
+
+function ()   :void
+{
+}
+
+function (
+    $a,
+    $c,
+    $d,
+    $e,
+    $b
+):void {
+}
+
+function (
+    $a,
+    $c,
+    $d,
+    $e,
+    $b
+) :void {
+}
+
+function (
+    $a,
+    $c,
+    $d,
+    $e,
+    $b
+)   :void {
+}
+
+function (
+    $a,
+    $c,
+    $d,
+    $e,
+    $b
+): void {
+}
+
+function (
+    $a,
+    $c,
+    $d,
+    $e,
+    $b
+):   void {
+}
