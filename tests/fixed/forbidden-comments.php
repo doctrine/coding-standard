b33c0a5e08ce9b4d3f675134d6fe5cd90ba96eaa
Michael Moravec <mail@majkl578.cz>
2018-02-02 03:03:22 +0100

    Enable ForbiddenComments sniff

---
+++ tests/fixed/forbidden-comments.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test;
+
+class Foo
+{
+    public function __construct()
+    {
+        echo 'Hello';
+    }
+
+    public function getBar() : int
+    {
+        return 123;
+    }
+
+    /**
+     * Very important getter.
+     */
+    public function getBaz() : int
+    {
+        return 456;
+    }
+}
