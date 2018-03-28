0ddfc831a85bae77342bdd0a77b583704d9ab142
Gabriel Caruso <carusogabriel34@gmail.com>
2018-03-28 19:55:04 -0300

    Forbidden usage of extract

--- tests/fixed/forbidden-functions.php
+++ tests/fixed/forbidden-functions.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Test;
 
 use function chop;
+use function extract;
 use function is_null;
 use function settype;
 use function sizeof;
@@ -17,3 +18,10 @@ echo is_null(456) ? 'y' : 'n';
 $foo = '1';
 settype($foo, 'int');
 var_dump($foo);
+
+$bar = [
+    'foo' => 1,
+    'bar' => 2,
+    'baz' => 3,
+];
+extract($bar);
