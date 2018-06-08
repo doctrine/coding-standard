06def935b3c960de58af04667b5a1783e2a4cb6b
Claudio Zizza <claudio@budgegeria.de>
2018-06-08 22:53:38 +0200

    Make FooException of input-classes dirty

--- tests/input/FooException.php
+++ tests/input/FooException.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace Example;
 
-use Namespacing\Classname;
-use Throwable;
-
-interface FooException extends Throwable, \Namespacing\Test, Classname
+interface FooException
 {
 }
