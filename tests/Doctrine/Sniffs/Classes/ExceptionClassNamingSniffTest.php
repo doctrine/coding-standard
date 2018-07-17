480c8fde7d9eed47590e00838a6125cdca69d04a
Claudio Zizza <claudio@budgegeria.de>
2018-07-17 22:02:52 +0200

    Set codesniffer coding style for tests

--- tests/Doctrine/Sniffs/Classes/ExceptionClassNamingSniffTest.php
+++ tests/Doctrine/Sniffs/Classes/ExceptionClassNamingSniffTest.php
@@ -1,19 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Doctrine\Sniffs\Classes;
 
 use SlevomatCodingStandard\Sniffs\TestCase;
 
 class ExceptionClassNamingSniffTest extends TestCase
 {
-    private const PATH_TO_CLASSES = __DIR__.'/../../../test/exception-class/';
+    private const PATH_TO_CLASSES = __DIR__ . '/../../../test/exception-class/';
 
     /**
      * @dataProvider provideValidInterfaceFiles
-     *
-     * @param string $filePath
      */
-    public function testValidClass(string $filePath)
+    public function testValidClass(string $filePath) : void
     {
         $phpcsFile = self::checkFile($filePath);
         self::assertNoSniffErrorInFile($phpcsFile);
@@ -21,10 +21,8 @@ class ExceptionClassNamingSniffTest extends TestCase
 
     /**
      * @dataProvider provideInvalidInterfaceFiles
-     *
-     * @param string $filePath
      */
-    public function testInvalidClass(string $filePath, int $line)
+    public function testInvalidClass(string $filePath, int $line) : void
     {
         $phpcsFile = self::checkFile($filePath);
 
@@ -32,28 +30,34 @@ class ExceptionClassNamingSniffTest extends TestCase
         self::assertSniffError($phpcsFile, $line, 'NotAnExceptionClass');
     }
 
+    /**
+     * @return string[][]
+     */
     public function provideValidInterfaceFiles() : array
     {
         return [
-            'Extends exception' => [self::PATH_TO_CLASSES.'ValidA.php'],
-            'Implements exception' => [self::PATH_TO_CLASSES.'ValidB.php'],
-            'Extends fqcn exception' => [self::PATH_TO_CLASSES.'ValidC.php'],
-            'Implements fqcn exception' => [self::PATH_TO_CLASSES.'ValidD.php'],
-            'Implements Throwable' => [self::PATH_TO_CLASSES.'ValidE.php'],
-            'No exception' => [self::PATH_TO_CLASSES.'ValidF.php'],
-            'Abstract extends exception' => [self::PATH_TO_CLASSES.'ValidAException.php'],
-            'Abstract implements exception' => [self::PATH_TO_CLASSES.'ValidBException.php'],
+            'Extends exception' => [self::PATH_TO_CLASSES . 'ValidA.php'],
+            'Implements exception' => [self::PATH_TO_CLASSES . 'ValidB.php'],
+            'Extends fqcn exception' => [self::PATH_TO_CLASSES . 'ValidC.php'],
+            'Implements fqcn exception' => [self::PATH_TO_CLASSES . 'ValidD.php'],
+            'Implements Throwable' => [self::PATH_TO_CLASSES . 'ValidE.php'],
+            'No exception' => [self::PATH_TO_CLASSES . 'ValidF.php'],
+            'Abstract extends exception' => [self::PATH_TO_CLASSES . 'ValidAException.php'],
+            'Abstract implements exception' => [self::PATH_TO_CLASSES . 'ValidBException.php'],
         ];
     }
 
+    /**
+     * @return string[][]|int[][]
+     */
     public function provideInvalidInterfaceFiles() : array
     {
         return [
-            'Class with exception suffix' => [self::PATH_TO_CLASSES.'NotAbstractException.php', 7],
-            'Inherits no other class or interface' => [self::PATH_TO_CLASSES.'InheritsNothingException.php', 7],
-            'Abstract without exception suffix' => [self::PATH_TO_CLASSES.'AbstractWithoutSuffix.php', 7],
-            'Throwable in same namespace' => [self::PATH_TO_CLASSES.'ThrowableSameNamespaceException.php', 7],
-            'Implements different throwable' => [self::PATH_TO_CLASSES.'DifferentThrowableException.php', 9],
+            'Class with exception suffix' => [self::PATH_TO_CLASSES . 'NotAbstractException.php', 7],
+            'Inherits no other class or interface' => [self::PATH_TO_CLASSES . 'InheritsNothingException.php', 7],
+            'Abstract without exception suffix' => [self::PATH_TO_CLASSES . 'AbstractWithoutSuffix.php', 7],
+            'Throwable in same namespace' => [self::PATH_TO_CLASSES . 'ThrowableSameNamespaceException.php', 7],
+            'Implements different throwable' => [self::PATH_TO_CLASSES . 'DifferentThrowableException.php', 9],
         ];
     }
 }
--- tests/Doctrine/Sniffs/Classes/ExceptionInterfaceNamingSniffTest.php
+++ tests/Doctrine/Sniffs/Classes/ExceptionInterfaceNamingSniffTest.php
@@ -1,19 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Doctrine\Sniffs\Classes;
 
 use SlevomatCodingStandard\Sniffs\TestCase;
 
 class ExceptionInterfaceNamingSniffTest extends TestCase
 {
-    private const PATH_TO_CLASSES = __DIR__.'/../../../test/exception-interface/';
+    private const PATH_TO_CLASSES = __DIR__ . '/../../../test/exception-interface/';
 
     /**
      * @dataProvider provideValidInterfaceFiles
-     *
-     * @param string $filePath
      */
-    public function testValidInterface(string $filePath)
+    public function testValidInterface(string $filePath) : void
     {
         $phpcsFile = self::checkFile($filePath);
         self::assertNoSniffErrorInFile($phpcsFile);
@@ -21,10 +21,8 @@ class ExceptionInterfaceNamingSniffTest extends TestCase
 
     /**
      * @dataProvider provideInvalidInterfaceFiles
-     *
-     * @param string $filePath
      */
-    public function testInvalidInterface(string $filePath, int $line)
+    public function testInvalidInterface(string $filePath, int $line) : void
     {
         $phpcsFile = self::checkFile($filePath);
 
@@ -32,30 +30,36 @@ class ExceptionInterfaceNamingSniffTest extends TestCase
         self::assertSniffError($phpcsFile, $line, 'NotAnException');
     }
 
+    /**
+     * @return string[][]
+     */
     public function provideValidInterfaceFiles() : array
     {
         return [
-            'Imports and uses Throwable' => [self::PATH_TO_CLASSES.'ValidAException.php'],
-            'Uses Throwable' => [self::PATH_TO_CLASSES.'ValidBException.php'],
-            'Imports and uses exception interface' => [self::PATH_TO_CLASSES.'ValidCException.php'],
-            'Uses exception interface' => [self::PATH_TO_CLASSES.'ValidDException.php'],
-            'Combination of interfaces' => [self::PATH_TO_CLASSES.'ValidEException.php'],
-            'Not relevant for the sniff' => [self::PATH_TO_CLASSES.'NoSuffixAndInterface.php'],
-            'Throwable namespace alias' => [self::PATH_TO_CLASSES.'ValidFException.php'],
-            'Exception namespace alias' => [self::PATH_TO_CLASSES.'ValidGException.php'],
-            'Comma separated namespaces' => [self::PATH_TO_CLASSES.'ValidHException.php'],
-            'Group use namespaces' => [self::PATH_TO_CLASSES.'ValidIException.php'],
+            'Imports and uses Throwable' => [self::PATH_TO_CLASSES . 'ValidAException.php'],
+            'Uses Throwable' => [self::PATH_TO_CLASSES . 'ValidBException.php'],
+            'Imports and uses exception interface' => [self::PATH_TO_CLASSES . 'ValidCException.php'],
+            'Uses exception interface' => [self::PATH_TO_CLASSES . 'ValidDException.php'],
+            'Combination of interfaces' => [self::PATH_TO_CLASSES . 'ValidEException.php'],
+            'Not relevant for the sniff' => [self::PATH_TO_CLASSES . 'NoSuffixAndInterface.php'],
+            'Throwable namespace alias' => [self::PATH_TO_CLASSES . 'ValidFException.php'],
+            'Exception namespace alias' => [self::PATH_TO_CLASSES . 'ValidGException.php'],
+            'Comma separated namespaces' => [self::PATH_TO_CLASSES . 'ValidHException.php'],
+            'Group use namespaces' => [self::PATH_TO_CLASSES . 'ValidIException.php'],
         ];
     }
 
+    /**
+     * @return string[][]|int[][]
+     */
     public function provideInvalidInterfaceFiles() : array
     {
         return [
-            'Missing exception suffix' => [self::PATH_TO_CLASSES.'NoSuffix.php', 9],
-            'Extends no exception' => [self::PATH_TO_CLASSES.'NoExtendedException.php', 9],
-            'Extends nothing' => [self::PATH_TO_CLASSES.'ExtendedsNothingException.php', 7],
-            'Wrong throwable' => [self::PATH_TO_CLASSES.'DifferentThrowableException.php', 9],
-            'Throwable in same namespace' => [self::PATH_TO_CLASSES.'ThrowableSameNamespaceException.php', 7],
+            'Missing exception suffix' => [self::PATH_TO_CLASSES . 'NoSuffix.php', 9],
+            'Extends no exception' => [self::PATH_TO_CLASSES . 'NoExtendedException.php', 9],
+            'Extends nothing' => [self::PATH_TO_CLASSES . 'ExtendedsNothingException.php', 7],
+            'Wrong throwable' => [self::PATH_TO_CLASSES . 'DifferentThrowableException.php', 9],
+            'Throwable in same namespace' => [self::PATH_TO_CLASSES . 'ThrowableSameNamespaceException.php', 7],
         ];
     }
 }
