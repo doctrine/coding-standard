67e583667c020aa71b881cf5e9b57606a5797b30
Michael Moravec <me@majkl.me>
2019-02-08 20:13:47 +0100

    Enable UselessConditionWithReturnSniff

---
+++ tests/input/continue-in-switch.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+foreach ([1, 2, 3] as $i) {
+    switch ($i) {
+        case 1:
+            continue;
+        case 2:
+            continue 2;
+        case 3:
+            break;
+    }
+
+    foo();
+}
