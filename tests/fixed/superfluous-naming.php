94e24f9462d2fac4f95e606acc6c8fbf344bf814
Michael Moravec <me@majkl.me>
2019-02-08 23:56:07 +0100

    Enable SuperfluousTraitNamingSniff

---
+++ tests/fixed/superfluous-naming.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test;
+
+abstract class AbstractFoo
+{
+}
+
+class FooException extends RuntimeException
+{
+}
+
+interface FooInterface
+{
+}
+
+trait FooTrait
+{
+}
