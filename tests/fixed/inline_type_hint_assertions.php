c5873a5c904a50ac316ab5b84afce4ed3e2af2c7
Marco Pivetta <ocramius@gmail.com>
2019-04-26 23:27:31 +0200

    Rewrote `inline_type_hint_assertions` to just be procedural PHP
    No classes, namespaces nor other symbols involved. Also got rid of any
    other CS rule broken by the file, limiting it to `/** @var foo $foo */`
    scenarios.
    
    This patch requires the new experimental `SlevomatCodingStandard.PHP.RequireExplicitAssertion`
    rule, which is available as per https://github.com/slevomat/coding-standard/commit/a2d0d67dd7e57b0dfbf125b6651b5303e62520c0

--- tests/fixed/inline_type_hint_assertions.php
+++ tests/fixed/inline_type_hint_assertions.php
@@ -2,31 +2,25 @@
 
 declare(strict_types=1);
 
-final class Foo
-{
-    public function methodWithInlineHints()
-    {
-        $simpleType = expression();
-        assert($simpleType instanceof Type);
+$simpleType = expression();
+assert($simpleType instanceof Type);
 
-        $typeDeclaredAfterExpression = expression();
-        assert($typeDeclaredAfterExpression instanceof Type);
+$typeDeclaredAfterExpression = expression();
+assert($typeDeclaredAfterExpression instanceof Type);
 
-        $typeDeclaredViaAssertion = expression();
-        assert($typeDeclaredViaAssertion instanceof Type);
+$typeDeclaredViaAssertion = expression();
+assert($typeDeclaredViaAssertion instanceof Type);
 
-        $unionType = expression();
-        assert($unionType instanceof Type1 || $unionType instanceof Type2);
+$unionType = expression();
+assert($unionType instanceof Type1 || $unionType instanceof Type2);
 
-        $intersectionType = expression();
-        assert($intersectionType instanceof Type1 && $intersectionType instanceof Type2);
+$intersectionType = expression();
+assert($intersectionType instanceof Type1 && $intersectionType instanceof Type2);
 
-        $nullableType = expression();
-        assert($nullableType instanceof Type || $nullableType === null);
+$nullableType = expression();
+assert($nullableType instanceof Type || $nullableType === null);
 
-        $multipleScalarTypes = expression();
-        assert(is_int($multipleScalarTypes) || is_float($multipleScalarTypes) || is_bool($multipleScalarTypes) || is_string($multipleScalarTypes) || $multipleScalarTypes === null || is_array($multipleScalarTypes));
+$multipleScalarTypes = expression();
+assert(is_int($multipleScalarTypes) || is_float($multipleScalarTypes) || is_bool($multipleScalarTypes) || is_string($multipleScalarTypes) || is_array($multipleScalarTypes) || $multipleScalarTypes === null);
 
-        assert($variableThatIsNowhereToBeFound instanceof Potato);
-    }
-}
+assert($variableThatIsNowhereToBeFound instanceof Potato);
--- tests/input/inline_type_hint_assertions.php
+++ tests/input/inline_type_hint_assertions.php
@@ -2,31 +2,25 @@
 
 declare(strict_types=1);
 
-final class Foo
-{
-    public function methodWithInlineHints()
-    {
-        /** @var Type $simpleType */
-        $simpleType = expression();
+/** @var Type $simpleType */
+$simpleType = expression();
 
-        $typeDeclaredAfterExpression = expression();
-        /** @var Type $typeDeclaredAfterExpression */
+$typeDeclaredAfterExpression = expression();
+/** @var Type $typeDeclaredAfterExpression */
 
-        $typeDeclaredViaAssertion = expression();
-        assert($typeDeclaredViaAssertion instanceof Type);
+$typeDeclaredViaAssertion = expression();
+assert($typeDeclaredViaAssertion instanceof Type);
 
-        /** @var Type1|Type2 $unionType */
-        $unionType = expression();
+/** @var Type1|Type2 $unionType */
+$unionType = expression();
 
-        /** @var Type1&Type2 $intersectionType */
-        $intersectionType = expression();
+/** @var Type1&Type2 $intersectionType */
+$intersectionType = expression();
 
-        /** @var Type|null $nullableType */
-        $nullableType = expression();
+/** @var Type|null $nullableType */
+$nullableType = expression();
 
-        /** @var int|float|bool|string|null|array $multipleScalarTypes */
-        $multipleScalarTypes = expression();
+/** @var int|float|bool|string|array|null $multipleScalarTypes */
+$multipleScalarTypes = expression();
 
-        /** @var Potato $variableThatIsNowhereToBeFound */
-    }
-}
+/** @var Potato $variableThatIsNowhereToBeFound */
