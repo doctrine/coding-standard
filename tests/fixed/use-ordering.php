1ede95a73c1319e9a75c340ad4a14751c1fa7a79
Gabriel Caruso <carusogabriel34@gmail.com>
2020-03-20 17:52:31 +0100

    Adopt PSR-12 for imports statements separation
    [PSR-12](https://www.php-fig.org/psr/psr-12) is an extension of the PSR-2 set of rules
    that aims to standardize the code style.
    
    As per PSR-12, under the [item 3](https://www.php-fig.org/psr/psr-12/#3-declare-statements-namespace-and-import-statements):
    
    The header of a PHP file may consist of a number of different blocks. If present,
    each of the blocks below MUST be separated by a single blank line, and MUST NOT
    contain a blank line. Each block MUST be in the order listed below, although
    blocks that are not relevant may be omitted.
    
    For context on why this was voted in the past, see #9.

--- tests/fixed/use-ordering.php
+++ tests/fixed/use-ordering.php
@@ -6,7 +6,9 @@ namespace Foo;
 
 use DateTimeImmutable;
 use DateTimeInterface;
+
 use function sprintf;
+
 use const PHP_EOL;
 
 echo sprintf('Current date and time is %s', (new DateTimeImmutable())->format(DateTimeInterface::ATOM)) . PHP_EOL;
--- tests/input/example-class.php
+++ tests/input/example-class.php
@@ -5,9 +5,13 @@ declare(strict_types = 1);
 namespace Example;
 
 use Throwable;
+
 use function strlen as stringLength;
+
 use Fancy\TestCase as TestCase;
+
 use const PHP_RELEASE_VERSION as PHP_PATCH_VERSION;
+
 use Doctrine\Sniffs\Spacing\ControlStructureSniff;
 
 /**
