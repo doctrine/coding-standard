58ee6a2415873d085dc29ebbd8eedf717602237b
Gabriel Caruso <carusogabriel34@gmail.com>
2020-03-22 17:44:47 +0100

    Ensure that Typed Properties are spaced correctly
    This new rule checks for things like:
    
    - no space between the typehint and the property name
    - no space between the typehint and the property's modifiers
    - no space between the nullability symbol and the typehint
    
    This rule does not check for:
    
    - How many lines the typed properties are separated by
    - If two consecutive properties have default values, the `=` sign will be aligned# Please enter the commit message for your changes. Lines starting

---
+++ tests/input/PropertyTypeHintSpacing.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spacing;
+
+final class PropertyTypeHintSpacing
+{
+    public bool $boolPropertyWithDefaultValue  = false;
+    public string  $stringProperty;
+    public  int $intProperty;
+    public ? string $nullableString = null;
+}
