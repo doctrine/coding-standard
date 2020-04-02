c4411848a8b041e69bbdc0346d7a3c4f0ca89f1f
Andreas Braun <git@alcaeus.org>
2020-04-02 13:38:16 +0200

    Add sniff to check multiline method call annotation

---
+++ tests/fixed/multiline-call-indentation.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+$object = new stdClass();
+
+$object->method();
+
+$object->method()->nested();
+
+$object
+    ->method()
+    ->nested();
+
+$object
+    ->method()
+        ->doubleNested()
+        ->stillNested()
+            ->nestedTooFar()
+        ->evenMoreLevels()
+    ->notNested()
+        ->nestedCorrectly();
