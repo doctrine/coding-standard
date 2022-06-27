94ec677fa4cf958e23510b65743a613b821ce904
Simon Podlipsky <simon@podlipsky.net>
2022-06-27 10:22:52 +0300

    feat: enforce spaces between constants

--- tests/fixed/constants-var.php
+++ tests/fixed/constants-var.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace ConstantsVar;
 
+// phpcs:disable PSR1.Classes.ClassDeclaration.MultipleClasses, Squiz.Classes.ClassFileName.NoMatch
+
 const FOO = 123;
 
 const BAR_1 = 1;
@@ -15,4 +17,15 @@ class Bar
 
     protected const PROPERTY_1 = '1';
     protected const PROPERTY_2 = '2';
+}
+
+class Spacing
+{
+    public const FOO = 'bar';
+    public const BAR = 'bar';
+
+    public const BAZ = 'baz';
+
+    /** Brevis, primus coordinataes foris promissio de varius, barbatus heuretes. */
+    private const BAM = 1234;
 }
