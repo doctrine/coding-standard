622d79dac4856a9a50a7e04eee49a284991627c7
Grégoire Paris <postmaster@greg0ire.fr>
2022-08-26 09:24:21 +0200

    Revert "chore(deps): require slevomat/coding-standard v8 and squizlabs/php_codesniffer v3.7"
    This reverts commit fd9171ded2fe711bd2e7134253f3e8f2340bc394.
    This should not have gone in a patch release.

--- tests/input/PropertyTypeHintSpacing.php
+++ tests/input/PropertyTypeHintSpacing.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Spacing;
 
-final class PropertyDeclaration
+final class PropertyTypeHintSpacing
 {
     public bool $boolPropertyWithDefaultValue  = false;
     public string  $stringProperty;
