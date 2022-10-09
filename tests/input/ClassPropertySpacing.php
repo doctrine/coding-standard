ecc67467b52f640885602dc5e55eac22e20ef0e7
Simon Podlipsky <simon@podlipsky.net>
2022-10-09 11:04:11 +0200

    feat: handle blank lines between class properties

---
+++ tests/input/ClassPropertySpacing.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spacing;
+
+final class ClassPropertySpacing
+{
+    public bool $foo;
+
+
+    /** @var string[] */
+    public array $bar;
+    /** @var string[] */
+    public array $baz;
+    public int $qux;
+
+
+    public int $fred;
+}
