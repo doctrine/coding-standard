c140fc0e5039a55cedf0de5ab53ada245f56db49
Alexander M. Turek <me@derrabus.de>
2022-12-11 10:29:04 +0100

    String interpolation: don't suggest sprintf()

--- tests/fixed/strings.php
+++ tests/fixed/strings.php
@@ -7,3 +7,13 @@ namespace Strings;
 $doc = <<<'TEXT'
 text without special chars
 TEXT;
+
+$interpolated = <<<TEXT
+HEREDOC with interpolation like $doc is fine.
+TEXT;
+
+echo 'This should appear in single quotes.';
+
+echo "This string\tmay appear\nin double quotes.";
+
+echo "String interpolation like $doc should be avoided.";
