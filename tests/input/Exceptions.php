3770eb2a9b12c82da9258d2db34c6a2257301f6b
Simon Podlipsky <simon@podlipsky.net>
2026-06-30 09:06:16 +0300

    feat(rules): enforce catch exception order

--- tests/input/Exceptions.php
+++ tests/input/Exceptions.php
@@ -5,9 +5,16 @@ declare(strict_types=1);
 namespace Exceptions;
 
 use Exception;
+use LogicException;
+use RuntimeException;
 use Throwable;
 
 try {
     throw new Exception();
 } catch (Throwable $throwable) {
+}
+
+try {
+    throw new LogicException();
+} catch (RuntimeException | LogicException) {
 }
